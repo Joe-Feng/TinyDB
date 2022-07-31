579464bf3c48fff867d8dc7f88541361ec8922dd
Feng <1322593809@qq.com>
2022-07-31 13:11:39 +0800

    feat: 客户端与服务端的实现
    1. Server 启动一个 ServerSocket 监听端口，当有请求到来时直接把请求丢给一个新线程处理
    2. 客户端有一个简单的Shell，实际上只是读入用户的输入，并调用 Client.execute()

---
+++ src/main/java/client/RoundTripper.java
@@ -0,0 +1,24 @@
+package client;
+
+import transport.Packager;
+import transport.Package;
+
+/**
+ * 实现了单次收发动作
+ */
+public class RoundTripper {
+    private Packager packager;
+
+    public RoundTripper(Packager packager) {
+        this.packager = packager;
+    }
+
+    public Package roundTrip(Package pkg) throws Exception {
+        packager.send(pkg);
+        return packager.receive();
+    }
+
+    public void close() throws Exception {
+        packager.close();
+    }
+}
---
+++ src/main/java/client/Shell.java
@@ -0,0 +1,38 @@
+package client;
+
+import java.util.Scanner;
+
+/**
+ * 一个简单的 Shell，实际上只是读入用户的输入，并调用 Client.execute()。
+ */
+public class Shell {
+    private Client client;
+
+    public Shell(Client client) {
+        this.client = client;
+    }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        try {
+            while(true) {
+                System.out.print(":> ");
+                String statStr = sc.nextLine();
+                if("exit".equals(statStr) || "quit".equals(statStr)) {
+                    break;
+                }
+                try {
+                    // 关键：调用client.execute
+                    byte[] res = client.execute(statStr.getBytes());
+                    System.out.println(new String(res));
+                } catch(Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+        } finally {
+            sc.close();
+            client.close();
+        }
+    }
+}
