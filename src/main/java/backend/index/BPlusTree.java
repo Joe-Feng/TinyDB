4853f795bc300804f91fc023fc5d2ac18f7ad54f
Feng <1322593809@qq.com>
2022-07-31 12:13:12 +0800

    feat: 索引管理
    1. 实现了基于 B+ 树的聚簇索引
    2. 目前只支持基于索引查找数据，不支持全表扫描

---
+++ src/main/java/backend/index/BPlusTree.java
@@ -0,0 +1,181 @@
+package backend.index;
+
+import backend.common.SubArray;
+import backend.data.DataManager;
+import backend.data.dataItem.DataItem;
+import backend.transaction.TransactionManagerImpl;
+import backend.utils.Parser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * IM 对上层模块主要提供两种能力：插入索引和搜索节点
+ *
+ * IM 为什么不提供删除索引的能力?
+ * 当上层模块通过 VM 删除某个 Entry，实际的操作是设置其 XMAX。
+ * 如果不去删除对应索引的话，当后续再次尝试读取该 Entry 时，是可以通过索引寻找到的，
+ * 但是由于设置了 XMAX，寻找不到合适的版本而返回一个找不到内容的错误。
+ *
+ * 节点间错误：
+ * 某次对 u 节点的插入操作创建了新节点 v, 此时 sibling(u)=v，
+ * 但是 v 却并没有被插入到父节点中。
+ *
+ * 改正错误：
+ * 如果要对节点进行插入或者搜索操作，如果失败，就会继续迭代它的兄弟节点，最终还是可以找到 v 节点。
+ * 唯一的缺点仅仅是，无法直接通过父节点找到 v 了，只能间接地通过 u 获取到 v。
+ */
+public class BPlusTree {
+    DataManager dm;
+    long bootUid;
+    /**
+     * 由于 B+ 树在插入删除时，会动态调整，根节点不是固定节点，于是设置一个 bootDataItem
+     * 该 DataItem 中存储了根节点的 UID
+     */
+    DataItem bootDataItem;
+    Lock bootLock;
+
+    public static long create(DataManager dm) throws Exception {
+        byte[] rawRoot = Node.newNilRootRaw();
+        long rootUid = dm.insert(TransactionManagerImpl.SUPER_XID, rawRoot);
+        return dm.insert(TransactionManagerImpl.SUPER_XID, Parser.long2Byte(rootUid));
+    }
+
+    public static BPlusTree load(long bootUid, DataManager dm) throws Exception {
+        DataItem bootDataItem = dm.read(bootUid);
+        assert bootDataItem != null;
+        BPlusTree t = new BPlusTree();
+        t.bootUid = bootUid;
+        t.dm = dm;
+        t.bootDataItem = bootDataItem;
+        t.bootLock = new ReentrantLock();
+        return t;
+    }
+
+    private long rootUid() {
+        bootLock.lock();
+        try {
+            SubArray sa = bootDataItem.data();
+            return Parser.parseLong(Arrays.copyOfRange(sa.raw, sa.start, sa.start+8));
+        } finally {
+            bootLock.unlock();
+        }
+    }
+
+    private void updateRootUid(long left, long right, long rightKey) throws Exception {
+        bootLock.lock();
+        try {
+            byte[] rootRaw = Node.newRootRaw(left, right, rightKey);
+            long newRootUid = dm.insert(TransactionManagerImpl.SUPER_XID, rootRaw);
+            bootDataItem.before();
+            SubArray diRaw = bootDataItem.data();
+            System.arraycopy(Parser.long2Byte(newRootUid), 0, diRaw.raw, diRaw.start, 8);
+            bootDataItem.after(TransactionManagerImpl.SUPER_XID);
+        } finally {
+            bootLock.unlock();
+        }
+    }
+
+    private long searchLeaf(long nodeUid, long key) throws Exception {
+        Node node = Node.loadNode(this, nodeUid);
+        boolean isLeaf = node.isLeaf();
+        node.release();
+
+        if(isLeaf) {
+            return nodeUid;
+        } else {
+            long next = searchNext(nodeUid, key);
+            return searchLeaf(next, key);
+        }
+    }
+
+    private long searchNext(long nodeUid, long key) throws Exception {
+        while(true) {
+            Node node = Node.loadNode(this, nodeUid);
+            Node.SearchNextRes res = node.searchNext(key);
+            node.release();
+            if(res.uid != 0) {
+                return res.uid;
+            }
+            nodeUid = res.siblingUid;
+        }
+    }
+
+    public List<Long> search(long key) throws Exception {
+        return searchRange(key, key);
+    }
+
+    public List<Long> searchRange(long leftKey, long rightKey) throws Exception {
+        long rootUid = rootUid();
+        long leafUid = searchLeaf(rootUid, leftKey);
+        List<Long> uids = new ArrayList<>();
+        while(true) {
+            Node leaf = Node.loadNode(this, leafUid);
+            Node.LeafSearchRangeRes res = leaf.leafSearchRange(leftKey, rightKey);
+            leaf.release();
+            uids.addAll(res.uids);
+            if(res.siblingUid == 0) {
+                break;
+            } else {
+                leafUid = res.siblingUid;
+            }
+        }
+        return uids;
+    }
+
+    public void insert(long key, long uid) throws Exception {
+        long rootUid = rootUid();
+        InsertRes res = insert(rootUid, uid, key);
+        assert res != null;
+        if(res.newNode != 0) {
+            updateRootUid(rootUid, res.newNode, res.newKey);
+        }
+    }
+
+    class InsertRes {
+        long newNode, newKey;
+    }
+
+    private InsertRes insert(long nodeUid, long uid, long key) throws Exception {
+        Node node = Node.loadNode(this, nodeUid);
+        boolean isLeaf = node.isLeaf();
+        node.release();
+
+        InsertRes res = null;
+        if(isLeaf) {
+            res = insertAndSplit(nodeUid, uid, key);
+        } else {
+            long next = searchNext(nodeUid, key);
+            InsertRes ir = insert(next, uid, key);
+            if(ir.newNode != 0) {
+                res = insertAndSplit(nodeUid, ir.newNode, ir.newKey);
+            } else {
+                res = new InsertRes();
+            }
+        }
+        return res;
+    }
+
+    private InsertRes insertAndSplit(long nodeUid, long uid, long key) throws Exception {
+        while(true) {
+            Node node = Node.loadNode(this, nodeUid);
+            Node.InsertAndSplitRes iasr = node.insertAndSplit(uid, key);
+            node.release();
+            if(iasr.siblingUid != 0) {
+                nodeUid = iasr.siblingUid;
+            } else {
+                InsertRes res = new InsertRes();
+                res.newNode = iasr.newSon;
+                res.newKey = iasr.newKey;
+                return res;
+            }
+        }
+    }
+
+    public void close() {
+        bootDataItem.release();
+    }
+}
